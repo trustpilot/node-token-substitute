c869072b5179f8d58b59ec0efbbfbe4c92aaae54
martinbuberl <hello@martinbuberl.com>
2017-09-20 10:23:57 +0200

    Removed unused test data; renamed sub keys

--- test/test.js
+++ test/test.js
@@ -10,10 +10,8 @@ describe('Default option value tests', () => {
     test3: '#{key3-1}',
     test4: '#{key4.1}',
     test5: {
-      test1: '#{key5.1}',
-      test2: '#{key5.2}'
-    },
-    test6: '#{key1}'
+      test51: '#{key5.1}'
+    }
   };
 
   it('Test default options', () => {
@@ -22,7 +20,7 @@ describe('Default option value tests', () => {
     assert.notStrictEqual(actual, config);
     assert.equal(actual.test1, '');
     assert.equal(actual.test2, '{{key2}}');
-    assert.equal(actual.test5.test1, '');
+    assert.equal(actual.test5.test51, '');
   });
 
   it('Test preserveUnknownTokens set to true', () => {
@@ -30,7 +28,7 @@ describe('Default option value tests', () => {
     const actual = substitute(config, options);
     assert.notEqual(actual.test1, '', 'Expected to preserve value');
     assert.equal(actual.test2, '{{key2}}');
-    assert.notEqual(actual.test5.test1, '');
+    assert.notEqual(actual.test5.test51, '');
   });
 
   it('Test tokens is set', () => {
@@ -57,7 +55,7 @@ describe('Default option value tests', () => {
     assert.equal(actual.test1, 'value1');
     assert.equal(actual.test3, 'value3.1');
     assert.equal(actual.test4, 'value4.1');
-    assert.equal(actual.test5.test1, 'value5.1');
+    assert.equal(actual.test5.test51, 'value5.1');
   });
 
   it('Testing that the target can be string value', () => {
